c2d6dec24f8053328da3ead53f50c2b972867e17
Hulto <hulto@hul.to>
2021-11-17 00:25:52 -0500

    Set 1 Challenge 2 working.

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod utils;
 
 fn main() {
     println!("{:?}", utils::hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string()));
+    let c1 = "1c0111001f010100061a024b53535009181c".to_string();
+    let c2 = "686974207468652062756c6c277320657965".to_string();
+    println!("{:?}", utils::xor_bufs(c1,c2));
 }
 
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,30 @@ extern crate base64;
 use std::char;
 
 
+pub fn xor_bufs(c1: String, c2: String) -> String {
+    let mut res_string = "".to_string();
+    let c1string = hex_to_ascii_string(c1);
+    let c2string = hex_to_ascii_string(c2);
+    for i in 0..c1string.chars().count() {
+        let xor_res = (c1string.as_bytes()[i] ^ c2string.as_bytes()[i]) as char;
+        res_string = format!("{}{:x}", res_string, xor_res.to_string().as_bytes()[0]);
+    }
+    return res_string;
+}
+
+#[test]
+pub fn test_s1c2_xor_bufs() {
+    let c1 = "1c0111001f010100061a024b53535009181c".to_string();
+    let c2 = "686974207468652062756c6c277320657965".to_string();
+    let res = "746865206b696420646f6e277420706c6179".to_string();
+    assert_eq!(xor_bufs(c1,c2), res);
+}
+
+
+
+
+
+
 pub fn hex_to_ascii_string(hex_string: String) -> String {
     let mut ascii_string = "".to_string();
     for i in (0..hex_string.chars().count()-1).step_by(2) {
@@ -55,4 +79,3 @@ pub fn test_s1c1_hex_to_base64() {
     let res_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string();
     assert_eq!(hex_to_base64(hex_str), res_str);
 }
-
